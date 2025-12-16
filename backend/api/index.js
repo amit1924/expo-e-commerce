5370555753bcd6a4aeb6615ea1ec695af0077eaa
Amit Kumar <amit192400@gmail.com>
2025-12-16 12:57:31 +0530

    inngest code added

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,9 +2,16 @@ import express from 'express';
 
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { connectDB } from '../src/config/db.js';
+import { clerkMiddleware } from '@clerk/express';
+import { serve } from 'inngest/express';
+import { inngest, functions } from '../src/config/inngest.js';
 dotenv.config();
 
 const app = express();
+
+// Clerk middleware
+app.use(clerkMiddleware());
 
 // CORS configuration
 app.use(
@@ -27,7 +34,9 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 
-// Sample route
+app.use('/api/inngest', serve({ client: inngest, functions }));
+
+// server status route
 app.get('/', (req, res) => {
   res.json({ message: 'Server is running' });
 });
@@ -41,4 +50,5 @@ app.use((err, req, res, next) => {
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
